ff05683df14b30aec03756f6b5b586985685864f
V.N.Anirudh Oruganti <oruganti@email.sc.edu>
2020-10-26 22:54:15 -0400

    Updated Dockerfile

---
+++ packages/homework4/src/subscriber.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import rospy
+from std_msgs.msg import Float32
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id(), data.data)
+
+def listener():
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber('total', Float32, callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
+
