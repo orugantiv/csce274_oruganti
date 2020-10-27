fd4763628c0a02cc2831e38711981986323bc1ae
V.N.Anirudh Oruganti <oruganti@email.sc.edu>
2020-10-27 01:19:17 -0400

    Updated Dockerfile

--- packages/homework4/src/publisher.py
+++ packages/homework4/src/publisher.py
@@ -5,9 +5,10 @@ from std_msgs.msg import Float32
 
 rospy.init_node('talker')
 
-publisher = rospy.Publisher('total', Float32, queue_size=1)
+publisher = rospy.Publisher("homework1/delta", Float32, queue_size=1)
 rate = rospy.Rate(15) # 3 Hz
 
 while not rospy.is_shutdown():
     publisher.publish(19)
     rate.sleep()
+
--- packages/homework4/src/subscriber.py
+++ packages/homework4/src/subscriber.py
@@ -3,11 +3,11 @@ import rospy
 from std_msgs.msg import Float32
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id(), data.data)
+    rospy.loginfo(rospy.get_caller_id() + " Output: %s", data.data)
 
 def listener():
     rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber('total', Float32, callback)
+    rospy.Subscriber("homework1/total", Float32, callback)
     rospy.spin()
 
 if __name__ == '__main__':
