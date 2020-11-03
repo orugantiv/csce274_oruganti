b1c9a393093551d9d0bca8f68097d48b44e3fb4f
V.N.Anirudh Oruganti <oruganti@email.sc.edu>
2020-11-03 15:12:35 -0500

    Updated Dockerfile

---
+++ packages/project3/src/circle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#i used class slides
+import rospy
+from std_msgs.msg import Float32
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + " Output: %s", data.data)
+
+def listener():
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber("homework1/total", Float32, callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
+
