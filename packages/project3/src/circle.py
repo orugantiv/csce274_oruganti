cec7bce2aa2ef435f9b2ed0825a0a940b41bd383
V.N.Anirudh Oruganti <oruganti@email.sc.edu>
2020-11-04 19:08:30 -0500

    update

--- packages/project3/src/circle.py
+++ packages/project3/src/circle.py
@@ -2,15 +2,16 @@
 #i used class slides
 import rospy
 from duckietown_msgs.msg import Twist2DStamped
+from time import sleep
 
-rospy.init_node('circle')
+def circle():
 
-publisher = rospy.Publisher("car_cmd_switch_node/cmd", Twist2DStamped, queue_size=1)
-msg= Twist2DStamped(header=None, v='0.1',omega='0')
-sleep(3)
-msg= Twist2DStamped(header=None, v='0.1',omega='0')
+    pub = rospy.Publisher("car_cmd_switch_node/cmd", Twist2DStamped, queue_size=10)
+    rospy.init_node('Circle')
+    pub.publish(Twist2DStamped(header=None, v=0.5,omega=0.0))
+    sleep(10)
+    pub.publish(Twist2DStamped(header=None, v=0.0,omega=0))
 
-while not rospy.is_shutdown():
-    publisher.publish(msg)
-    rate.sleep()
+if __name__ == '__main__':
+    circle()
 
