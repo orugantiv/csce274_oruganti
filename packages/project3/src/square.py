d81982f62a6a9bcc3871e47787c9cbc922ea0e02
V.N.Anirudh Oruganti <oruganti@email.sc.edu>
2020-11-07 19:52:06 -0500

    update

--- packages/project3/src/square.py
+++ packages/project3/src/square.py
@@ -1,22 +1,42 @@
+#!/usr/bin/env python3
 import rospy
 from duckietown_msgs.msg import Twist2DStamped
 from time import sleep
 
 def square():
-    rotate=2.4
+    rotate=1.55
     stright=0.0
     velocity=0.4
-    count=0
-    while(count<4):
-        pub = rospy.Publisher("car_cmd_switch_node/cmd", Twist2DStamped, queue_size=10)
-        rospy.init_node('Square')
-        pub.publish(Twist2DStamped(header=None, v=0.4,omega=0.0))
-        sleep(2.5)
-        pub.publish(Twist2DStamped(header=None, v=0.0,omega=0))
-        sleep(1)
-        pub.publish(Twist2DStamped(header=None, v=0.0,omega=rotate))
-        sleep(1)
-        count+=1
+
+    pub = rospy.Publisher("car_cmd_switch_node/cmd", Twist2DStamped, queue_size=10)
+    rospy.init_node('Square')
+    pub.publish(Twist2DStamped(header=None, v=0.4,omega=0.0))
+    sleep(2.5)
+    pub.publish(Twist2DStamped(header=None, v=0.0,omega=rotate))
+    sleep(1)
+
+    pub = rospy.Publisher("car_cmd_switch_node/cmd", Twist2DStamped, queue_size=10)
+    rospy.init_node('Square')
+    pub.publish(Twist2DStamped(header=None, v=0.4,omega=0.0))
+    sleep(2.5)
+    pub.publish(Twist2DStamped(header=None, v=0.0,omega=rotate))
+    sleep(1)
+
+    pub = rospy.Publisher("car_cmd_switch_node/cmd", Twist2DStamped, queue_size=10)
+    rospy.init_node('Square')
+    pub.publish(Twist2DStamped(header=None, v=0.4,omega=0.0))
+    sleep(2.5)
+    pub.publish(Twist2DStamped(header=None, v=0.0,omega=rotate+0.2))
+    sleep(1)
+
+
+    pub = rospy.Publisher("car_cmd_switch_node/cmd", Twist2DStamped, queue_size=10)
+    rospy.init_node('Square')
+    pub.publish(Twist2DStamped(header=None, v=0.4,omega=0.0))
+    sleep(2.5)
+    pub.publish(Twist2DStamped(header=None, v=0.0,omega=rotate))
+    sleep(1)
+
     pub.publish(Twist2DStamped(header=None, v=0.0,omega=0))
 
 if __name__ == '__main__':
