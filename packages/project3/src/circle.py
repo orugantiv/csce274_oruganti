862020c84bf8a10473228d16f5a4566395f789cf
V.N.Anirudh Oruganti <oruganti@email.sc.edu>
2020-11-07 19:28:14 -0500

    update

--- packages/project3/src/circle.py
+++ packages/project3/src/circle.py
@@ -4,13 +4,18 @@ from duckietown_msgs.msg import Twist2DStamped
 from time import sleep
 
 def circle():
-
+    #On lecture slides "lec07.pdf", there was a formula given to find omega for the robot
+    #to go in a circle with a radius and choosen velocity. Formula omega=velocity/(radius-(length/2)) 
+    velocity=0.4# desired velocity
+    diameter=1# desired diameter of the circle in meters
+    radius=diameter/2#radius of the circle in meters
+    length=0.0762#length between two wheels in meters
+    omga=velocity/(radius-(length/2))
     pub = rospy.Publisher("car_cmd_switch_node/cmd", Twist2DStamped, queue_size=10)
     rospy.init_node('Circle')
-    pub.publish(Twist2DStamped(header=None, v=0.5,omega=0.0))
-    sleep(10)
+    pub.publish(Twist2DStamped(header=None, v=velocity,omega=omga))
+    sleep(15)
     pub.publish(Twist2DStamped(header=None, v=0.0,omega=0))
 
 if __name__ == '__main__':
     circle()
-
--- packages/project3/src/square.py
+++ packages/project3/src/square.py
@@ -1,14 +1,22 @@
-#!/usr/bin/env python3
 import rospy
 from duckietown_msgs.msg import Twist2DStamped
 from time import sleep
 
 def square():
-
-    pub = rospy.Publisher("car_cmd_switch_node/cmd", Twist2DStamped, queue_size=10)
-    rospy.init_node('Square')
-    pub.publish(Twist2DStamped(header=None, v=0.5,omega=0.0))
-    sleep(10)
+    rotate=2.4
+    stright=0.0
+    velocity=0.4
+    count=0
+    while(count<4):
+        pub = rospy.Publisher("car_cmd_switch_node/cmd", Twist2DStamped, queue_size=10)
+        rospy.init_node('Square')
+        pub.publish(Twist2DStamped(header=None, v=0.4,omega=0.2))
+        sleep(2.5)
+        pub.publish(Twist2DStamped(header=None, v=0.0,omega=0))
+        sleep(1)
+        pub.publish(Twist2DStamped(header=None, v=0.0,omega=rotate))
+        sleep(1)
+        count+=1
     pub.publish(Twist2DStamped(header=None, v=0.0,omega=0))
 
 if __name__ == '__main__':
