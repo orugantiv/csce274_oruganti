fb8dfe6ad57ed8c1ec5f3465b03e9388dd10f987
V.N.Anirudh Oruganti <oruganti@email.sc.edu>
2020-11-08 22:26:16 -0500

    update

--- packages/project3/src/square.py
+++ packages/project3/src/square.py
@@ -7,20 +7,21 @@ def square():
     pub = rospy.Publisher("car_cmd_switch_node/cmd", Twist2DStamped, queue_size=10)
     rospy.init_node('Square')
 
-    rotate=1.55
+    rotate=2.5
     stright=0.0
-    velocity=0.4
+    velocity=0.4099999964237213
     i=0
 
     while (i<4):
-        pub.publish(Twist2DStamped(header=None, v=velocity,omega=0.0))
+        pub.publish(Twist2DStamped(header=None, v=velocity,omega=0))
         sleep(2.5)
-        pub.publish(Twist2DStamped(header=None, v=0.0,omega=rotate))
-        sleep(1)
+        pub.publish(Twist2DStamped(header=None, v=0,omega=rotate))
+        sleep(1.5)
         i+=1
 
     pub.publish(Twist2DStamped(header=None, v=0.0,omega=0))
 
 if __name__ == '__main__':
     square()
+
 
