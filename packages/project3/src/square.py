aff912bcde5b299d91e814ae248492575d2216fc
V.N.Anirudh Oruganti <oruganti@email.sc.edu>
2020-11-08 19:41:56 -0500

    update

--- packages/project3/src/square.py
+++ packages/project3/src/square.py
@@ -4,38 +4,20 @@ from duckietown_msgs.msg import Twist2DStamped
 from time import sleep
 
 def square():
+    pub = rospy.Publisher("car_cmd_switch_node/cmd", Twist2DStamped, queue_size=10)
+    rospy.init_node('Square')
+
     rotate=1.55
     stright=0.0
     velocity=0.4
+    i=0
 
-    pub = rospy.Publisher("car_cmd_switch_node/cmd", Twist2DStamped, queue_size=10)
-    rospy.init_node('Square')
-    pub.publish(Twist2DStamped(header=None, v=0.4,omega=0.0))
-    sleep(2.5)
-    pub.publish(Twist2DStamped(header=None, v=0.0,omega=rotate))
-    sleep(1)
-
-    pub = rospy.Publisher("car_cmd_switch_node/cmd", Twist2DStamped, queue_size=10)
-    rospy.init_node('Square')
-    pub.publish(Twist2DStamped(header=None, v=0.4,omega=0.0))
-    sleep(2.5)
-    pub.publish(Twist2DStamped(header=None, v=0.0,omega=rotate))
-    sleep(1)
-
-    pub = rospy.Publisher("car_cmd_switch_node/cmd", Twist2DStamped, queue_size=10)
-    rospy.init_node('Square')
-    pub.publish(Twist2DStamped(header=None, v=0.4,omega=0.0))
-    sleep(2.5)
-    pub.publish(Twist2DStamped(header=None, v=0.0,omega=rotate+0.2))
-    sleep(1)
-
-
-    pub = rospy.Publisher("car_cmd_switch_node/cmd", Twist2DStamped, queue_size=10)
-    rospy.init_node('Square')
-    pub.publish(Twist2DStamped(header=None, v=0.4,omega=0.0))
-    sleep(2.5)
-    pub.publish(Twist2DStamped(header=None, v=0.0,omega=rotate))
-    sleep(1)
+    while (i<4):
+        pub.publish(Twist2DStamped(header=None, v=velocity,omega=0.0))
+        sleep(2.5)
+        pub.publish(Twist2DStamped(header=None, v=0.0,omega=rotate))
+        sleep(1)
+        i+=1
 
     pub.publish(Twist2DStamped(header=None, v=0.0,omega=0))
 
